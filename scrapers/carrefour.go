1b86103d2d78f13a8ef78d7bc53c463a9a1a3e5f
ferminsanchezb <112906153+ferminsanchezb@users.noreply.github.com>
2026-06-01 09:51:14 -0300

    Refactor Carrefour function to use api.Core

--- scrapers/carrefour.go
+++ scrapers/carrefour.go
@@ -1,14 +1,52 @@
 package scrapers
 
 import (
-	"ratoneando/cores/vtex"
+	"encoding/json"
+	"fmt"
+
+	"ratoneando/cores/api"
 	"ratoneando/products"
+	"ratoneando/utils/logger"
 )
 
 func Carrefour(query string) ([]products.Schema, error) {
-	return vtex.Core(vtex.CoreProps{
-		Query:   query,
-		BaseUrl: "https://www.carrefour.com.ar",
-		Source:  "carrefour",
+	return api.Core(api.CoreProps[ResponseStructure, RawProduct]{
+		Query:         query,
+		BaseUrl:       "https://www.carrefour.com.ar",
+		SearchPattern: func(q string) string { return "/api/catalog_system/pub/products/search/?ft=" + q },
+		Source:        "carrefour",
+		Normalizer: func(response ResponseStructure) []RawProduct {
+			var normalizedProducts []RawProduct
+			for _, rawProduct := range response {
+				var productData ProductData
+				if len(rawProduct.ProductData) == 0 {
+					continue
+				}
+				err := json.Unmarshal([]byte(rawProduct.ProductData[0]), &productData)
+				if err != nil {
+					logger.LogWarn(fmt.Sprintf("Error unmarshalling product data: %s", err))
+					continue
+				}
+				normalizedProducts = append(normalizedProducts, RawProduct{
+					ResponseProduct: rawProduct,
+					ProductData:     productData,
+				})
+			}
+			return normalizedProducts
+		},
+		Extractor: func(rawProduct RawProduct) products.ExtendedSchema {
+			return products.ExtendedSchema{
+				ID:          rawProduct.ProductId,
+				Source:      "carrefour",
+				Name:        rawProduct.ProductName,
+				Link:        rawProduct.Link,
+				Image:       rawProduct.Items[0].Images[0].ImageUrl,
+				Unavailable: !rawProduct.Items[0].Sellers[0].CommertialOffer.IsAvailable,
+				Price:       rawProduct.Items[0].Sellers[0].CommertialOffer.Price,
+				ListPrice:   rawProduct.Items[0].Sellers[0].CommertialOffer.ListPrice,
+				Unit:        rawProduct.MeasurementUnitUn,
+				UnitFactor:  rawProduct.UnitMultiplierUn,
+			}
+		},
 	})
 }
